e7f1395305e668e45a6a0c624e47393de996b26a
Al Jachimiak <aljachimiak@gmail.com>
2019-01-24 08:12:43 -0800

    makes growth field consistent across methods

--- src/lib/grow-your-own/move.js
+++ src/lib/grow-your-own/move.js
@@ -1,6 +1,6 @@
 class Move {
 	constructor(args) {
-		this.plantGrowth = args.plantGrowth || 0;
+		this.growth = args.growth || 0;
 		this.playerIndex = args.playerIndex || 0;
 		this.playerResources = args.playerResources || {};
 	}
