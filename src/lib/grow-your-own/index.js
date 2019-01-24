a5719634656fc5ac092637863c1ededee7026a02
Al Jachimiak <aljachimiak@gmail.com>
2019-01-24 08:14:03 -0800

    adds main methods for playing a game and taking turns

--- src/lib/grow-your-own/index.js
+++ src/lib/grow-your-own/index.js
@@ -9,24 +9,28 @@ module.exports = {
 	Dice,
 
 	takeTurn(args) {
-		let {game, playerIndex} = args;
+		let game = args.game;
+		const playerIndex = args.playerIndex;
+		const dist = Dice.roll(2);
+
+		game = GameAction.updatePlayerPosition({game, playerIndex, dist});
+		game = GameAction.applyNewResources({game, playerIndex});
+		game = GameAction.growPlant({game, playerIndex});
+		game = GameAction.recordMove({game, playerIndex});
+		game = GameAction.reduceResources({game, playerIndex});
+
+		return game;
+	},
+
+	playGame() {
+		const player = new Player('Jones', 'red');
+		let game = new Game({players: [player]});
+
+		while(!game.hasWinner) {
+			// only one player at this time
+			const playerIndex = 0;
+			game = this.takeTurn();
+		}
 		return game;
 	}
-}
-
-// takeTurn
-//		dice.roll(2)
-//		updatePlayerPosition
-//		applyNewResources
-//		growPlant
-
-//		new Move pushed to game.moves
-// 		needs game-action recordMove
-
-//		reduceResources
-//
-
-// playGame
-//		while (!game.hasWinner) {
-//			game = takeTurn(game)
-// }
+};
