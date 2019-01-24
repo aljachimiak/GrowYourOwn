93806e794ee2c08a9786ce2b2cd890e22ccf61cc
Al Jachimiak <aljachimiak@gmail.com>
2019-01-24 08:13:16 -0800

    completes game actions

--- src/lib/grow-your-own/game-action.js
+++ src/lib/grow-your-own/game-action.js
@@ -1,36 +1,18 @@
+import Move from './move';
+
 const GameAction = () => {};
 
-const determineNewResources = (args) => {
-	const {game, playerIndex} = args;
-	const tile = getBoardTile({game, playerIndex});
-	const type = tile.type;
+// updatePlayerPosition
+const updatePlayerPosition = (args) => {
+	const {game, dist, playerIndex} = args;
+	// todo add errors for missing args
+	game.players[playerIndex].position += dist;
+	return game;
+};
 
-	const defaultResources = {
-		sun: 0,
-		rain: 0,
-		fertilizer: 0
-	};
-
-	let resourceDiff;
-
-	if (type === 'robot') {
-		// robo-calibrate increases all resources to a minimum of 2
-		const minResources = robotTileEnforceMin({game, playerIndex});
-		
-		const regResources = robotTileRegulatePeaks({game, playerIndex});
-
-		resourceDiff = Object.assign(defaultResources, minResources, regResources);
-
-	} else {
-		// player gets resources
-		ressources = Object.assign({}, defaultResources);
-		resourceDiff[type] += dist;
-	}
-	return resourceDiff;
-}
-
+// applyNewResources
 const applyNewResources = (args) => {
-	const {game, playerIndex, dist} = args;
+	const {game, playerIndex} = args;
 	// todo add errors for missing args
 
 	if (game.players[playerIndex].position >= 36) {
@@ -46,24 +28,42 @@ const applyNewResources = (args) => {
 	return game;
 };
 
-const determinePlantGrowth = (args) => {
-	const {game, playerIndex} = args;
-	// todo - implment for real
-	return 1;
-}
+const resetPlayerPosition = (args) => {
+	// todo add errors for missing args
 
-const growPlant = (args) => {
 	const {game, playerIndex} = args;
-	const growthAmount = determinePlantGrowth({game, playerIndex});
-	// todo implement for real
-	game.players[playerIndex].growth += growthAmount;
-
-	if (game.players[playerIndex].growth >= 11) {
-		game.players[playerIndex].growth = 11; // maxHeight on plant is 11
-		game.hawWinner = true;
-	}
+	game.players[playerIndex].position -= 36;
 	return game;
 }
+
+const determineNewResources = (args) => {
+	const {game, playerIndex} = args;
+	const tile = getBoardTile({game, playerIndex});
+	const type = tile.type;
+
+	const defaultResources = {
+		sun: 0,
+		rain: 0,
+		fertilizer: 0
+	};
+
+	let newResources;
+
+	if (type === 'robot') {
+		// robo-calibrate increases all resources to a minimum of 2
+		const minResources = robotTileEnforceMin({game, playerIndex});
+		
+		const regResources = robotTileRegulatePeaks({game, playerIndex});
+
+		newResources = Object.assign(defaultResources, minResources, regResources);
+
+	} else {
+		// player gets resources
+		ressources = Object.assign({}, defaultResources);
+		newResources[type] += dist;
+	}
+	return newResources;
+};
 
 const getBoardTile = (args) => {
 	// todo add errors for missing args
@@ -72,21 +72,6 @@ const getBoardTile = (args) => {
 	const position = game.players[playerIndex].position;
 	return board[position];
 };
-
-const reduceResources = (args) => {
-	const {game, playerIndex} = args;
-	// todo implement
-
-	return game;
-};
-
-const resetPlayerPosition = (args) => {
-	// todo add errors for missing args
-
-	const {game, playerIndex} = args;
-	game.players[playerIndex].position -= 36;
-	return game;
-}
 
 const robotTileEnforceMin = (args) => {
 	const {game, playerIndex} = args;
@@ -115,16 +100,48 @@ const robotTileRegulatePeaks = (args) => {
 	return resources;
 };
 
-const updatePlayerPosition = (args) => {
-	const {game, dist, playerIndex} = args;
-	// todo add errors for missing args
-	game.players[playerIndex].position += dist;
-	return game;
-}
+// growPlant
+const growPlant = (args) => {
+	const {game, playerIndex} = args;
+	const growthAmount = determinePlantGrowth({game, playerIndex});
+	// todo implement for real
+	game.players[playerIndex].growth += growthAmount;
 
+	if (game.players[playerIndex].growth >= 11) {
+		game.players[playerIndex].growth = 11; // maxHeight on plant is 11
+		game.hasWinner = true;
+	}
+	return game;
+};
+
+const determinePlantGrowth = (args) => {
+	const {game, playerIndex} = args;
+	// todo - implment for real
+	return 1;
+};
+
+const recordMove = (args) => {
+	const {game, playerIndex} = args;
+	const growth = game.players[playerIndex].growth;
+	const playerResources = game.players[playerIndex].resources;
+
+	const move = new Move({playerIndex, growth, playerResources});
+
+	game.moves.push(move);
+	return game;
+};
+
+const reduceResources = (args) => {
+	const {game, playerIndex} = args;
+	// todo implement
+
+	return game;
+};
+
+GameAction.updatePlayerPosition = updatePlayerPosition;
 GameAction.applyNewResources = applyNewResources;
 GameAction.growPlant = growPlant;
-GameAction.updatePlayerPosition = updatePlayerPosition;
+GameAction.recordMove = recordMove;
 GameAction.reduceResources = reduceResources;
 
 // normally these would be private but we'll export them for testing
